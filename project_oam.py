5f82d1f8955f2a72bd34d52abe18246276b98a53
Guarav Saini <areyygaurav@mgail.com>
2024-02-14 17:51:01 +0530

    Detector

--- project_oam.py
+++ project_oam.py
@@ -8,6 +8,62 @@ from openai import OpenAI
 import random
 import cv2
 import requests
+import numpy as np
+
+def open_detector():
+    cap = cv2.VideoCapture(0)
+
+    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    classes = []
+    with open("coco.names", "r") as f:
+        classes = f.read().splitlines()
+
+    while True:
+        ret, frame = cap.read()
+        height, width, _ = frame.shape
+
+        blob = cv2.dnn.blobFromImage(frame, 1/255.0, (416, 416), swapRB=True, crop=False)
+        net.setInput(blob)
+        output_layers_names = net.getUnconnectedOutLayersNames()
+        layer_outputs = net.forward(output_layers_names)
+
+        boxes = []
+        confidences = []
+        class_ids = []
+        for output in layer_outputs:
+            for detection in output:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+        if len(indexes) > 0:
+            for i in indexes.flatten():
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                confidence = str(round(confidences[i], 2))
+                color = (0, 255, 0)
+                cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
+                cv2.putText(frame, f"{label} {confidence}", (x, y+20), cv2.FONT_HERSHEY_PLAIN, 1, color, 2)
+
+        cv2.imshow("Camera", frame)
+
+        if (cv2.waitKey(1) & 0xFF == ord('q')):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 TMDB_API_KEY = 'e46699161f0b7dd8356b06b96aa874f9'
 def get_movie_rating(movie_title):
@@ -225,7 +281,10 @@ if __name__ == '__main__':
                 speaker.speak(rating_info)
                 break
 
-
+            elif "open object detection" in query:
+                speaker.speak("Opening Detector sir")
+                open_detector()
+                break
 
             else:
                 chat(query)
